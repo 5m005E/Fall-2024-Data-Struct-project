410d0a0c4f603bb8979a04b5905399e8e3ba7725
santi-raigoza <santiago.raigoza00@gmail.com>
2024-11-15 13:04:43 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -1,6 +1,27 @@
-public class Trie {
+import java.util.HashMap;
 
-    public Trie () {
-        
+class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean isLeadNode;
+
+    TrieNode() {
+        this.isLeadNode = false;
     }
 }
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // method to insert into the trie
+    public void insert() {
+        
+    }
+
+    // method to get guesses/possible words 
+
+}
+
