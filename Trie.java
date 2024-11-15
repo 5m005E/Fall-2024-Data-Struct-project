6ed822bbee943568d6e73c478fa93f93e25dcec3
santi-raigoza <santiago.raigoza00@gmail.com>
2024-11-15 14:03:50 -0500

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
 
 class TrieNode {
-    HashMap<Character, TrieNode> children = new HashMap<>();
-    boolean isLeadNode;
+    HashMap<Character, TrieNode> children;
+    boolean isLeafNode;
 
     TrieNode() {
-        this.isLeadNode = false;
+        children = new HashMap<>();
+        this.isLeafNode = false;
     }
 }
 
@@ -17,8 +18,24 @@ public class Trie {
     }
 
     // method to insert into the trie
-    public void insert() {
-        
+    // pass in the whole word from words.txt
+    public void insert(String word) {
+        TrieNode currentNode = root; // start adding at the root node
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            // if it trie doesn't already contain the character then make a new branch
+            if (!currentNode.children.containsKey(c)); {
+                currentNode.children.put(c, new TrieNode());
+            }
+
+            // move to the next node/character 
+            currentNode = currentNode.children.get(c);
+            
+        }
+
+        // after the loop set the last node/character as a leaf node to know its a full word
+        currentNode.isLeafNode = true;
     }
 
     // method to get guesses/possible words 
